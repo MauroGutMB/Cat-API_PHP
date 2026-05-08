48d06f68a3be4ee9dfa69430800b0365a8466991
MauroGutMB <mgmbarros2006@gmail.com>
2026-05-08 16:59:51 -0300

    config

--- config.php
+++ config.php
@@ -7,6 +7,9 @@ if (isset($env['CAT_API_KEY'])) {
 	$catApiKey = $env['CAT_API_KEY'];
 }
 
+// Caso não haja .env, usar chave hardcoded. (jamais use isso em produção)
+// $catApiKey = "live_WTJvA8KE6SYgLR0USUe9kPdHM3KcstAcaXGchkMz7u9c9BSKh6pDhCbvwGHO0Ok0";
+
 
 $catApiBaseUrl = 'https://api.thecatapi.com/v1';
 
